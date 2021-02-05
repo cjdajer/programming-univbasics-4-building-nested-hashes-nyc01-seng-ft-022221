6e8e0ffe3760fcf145612a51c27bf03a5c619218
Christopher Dajer <chrisdajer@gmail.com>
2021-02-05 05:58:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,12 +47,7 @@ def monopoly_with_fourth_tier
   {
     railroads: {
       pieces: 4,
-      rent_in_dollars: {
-        one_piece_owned: 25,
-        two_piece_owned: 50,
-        three_piece_owned: 100,
-        four_piece_owned: 200,
-      },
+ 
       names: {
         reading_railroads: {mortgage_value: 100},
         pennsylvania_raildoas: {mortgage_value: 200},
